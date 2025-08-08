ed59ddcb96a2e7fc2477b59cf1a4a2d9c2f15957
wafa14161995 <99214239+wafa14161995@users.noreply.github.com>
2025-08-08 18:24:20 +0300

    Initial Commit

---
+++ Learning Journy App/Learning_Journy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Journy_AppApp.swift
+//  Learning Journy App
+//
+//  Created by Wafa Awad  on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Learning_Journy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
